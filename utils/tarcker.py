20e90dab6bfe5954ed22ea7f4e4621d332e045ba
Gleb <glebkuimov@gmail.com>
2024-12-19 18:05:48 +0300

    suka

--- utils/tarcker.py
+++ utils/tarcker.py
@@ -8,6 +8,9 @@ class Tracker():
     def __init__(self):
         self.chrome_options = Options()
         self.chrome_options.add_experimental_option("detach", True)
+        self.chrome_options.add_argument('--headless')
+        self.chrome_options.add_argument('--no-sandbox')
+        self.chrome_options.add_argument('--disable-dev-shm-usage')
         self.browser = webdriver.Chrome(self.chrome_options)
 
     async def track_led(self, awb_blank, awb_num, airport = None):
