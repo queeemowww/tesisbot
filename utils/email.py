9f33fb9cde7a81c8ba1ef3638a741582f3e8be71
Gleb <glebkuimov@gmail.com>
2024-12-20 16:59:51 +0300

    suak21

---
+++ utils/email.py
@@ -0,0 +1,12 @@
+# # Import smtplib for the actual sending function
+# import smtplib
+
+# # Import the email modules we'll need
+# from email.mime.text import MIMEText
+
+# class Send_order():
+#     def __init__(self):
+#         pass
+    
+#     def send(self, msg):
+#         msg = MIMEText()
